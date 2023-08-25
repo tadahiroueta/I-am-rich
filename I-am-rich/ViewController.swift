5981b0af764873551ec77e1e46fdac34728c5372
tadahiroueta <tadahiroueta@gmail.com>
2023-08-25 08:49:30 -0500

    Initial Commit

---
+++ I-am-rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I-am-rich
+//
+//  Created by Ueta, Lucas T on 8/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
